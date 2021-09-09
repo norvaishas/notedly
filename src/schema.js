c9dd233c497f8f319131a5afc10b7c73a33d59d6
Sergei Norvaishas <snorvaishas@yandex.ru>
2021-09-10 00:31:35 +0300

    7. Accounts and authentication. feat: add users schemas

--- src/schema.js
+++ src/schema.js
@@ -3,12 +3,21 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
-scalar DateTime
+  
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    avatar: String
+    notes: [Note!]!
+  }
+  
+  scalar DateTime
 
-type Note {
+  type Note {
     id: ID!
     content: String!
-    author: String!
+    author: User!
     createdAt: DateTime!
     updatedAt: DateTime!
   }
@@ -23,5 +32,7 @@ type Note {
     newNote(content: String!): Note!
     updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
+    signUp(username: String!, email: String!, password: String!): String!
+    signIn(username: String, email: String, password: String!): String!
   }
 `;
