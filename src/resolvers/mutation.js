3869a58cb558dd0640d83e12fd7ee32af536347f
Sergei Norvaishas <snorvaishas@yandex.ru>
2021-09-10 01:24:06 +0300

    7. Accounts and authentication. feat: implement signUp and signIn resolvers

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,4 +1,10 @@
 const models = require('../models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { AuthenticationError, ForbiddenError } = require('apollo-server-express');
+require('dotenv').config();
+
+const gravatar = require('../util/gravatar');
 
 module.exports = {
   newNote: async (parent, args) => {
@@ -22,5 +28,46 @@ module.exports = {
         $set: {content}
       }
     )
+  },
+  signUp: async (parent, { username, email, password }) => {
+    email = email.trim().toLowerCase();
+    // Хэшируем пароль
+    const hashed = await bcrypt.hash(password, 10);
+    // Создаем url gravatar-изображения
+    const avatar = gravatar(email);
+
+    try {
+      const user = await models.User.create({
+        username,
+        email,
+        avatar,
+        password: hashed
+      });
+    // Создаем и возвращаем json web token
+      return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    } catch (err) {
+      console.log(err);
+    // Если при регистрации возникла проблема, выбрасываем ошибку
+      throw new Error('Error creating account');
+    }
+  },
+  signIn: async (parent, { username, email, password }) => {
+    if (email) {
+      email = email.trim().toLowerCase();
+    }
+    const user = await models.User.findOne({
+      $or: [{ email }, { username }]
+    });
+    // Если пользователь не найден, выбрасываем ошибку аутентификации
+    if (!user) {
+      throw new AuthenticationError('Error signing in');
+    }
+    // Если пароли не совпадают, выбрасываем ошибку аутентификации
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AuthenticationError('Error signing in');
+    }
+    // Создаем и возвращаем json web token
+    return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   }
 };
